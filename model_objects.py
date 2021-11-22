b3bc5eb320e178e75e5859f0b937acfe2acb2afc
Daniil Koblov <koblov.da@phystech.edu>
2021-11-22 23:34:54 +0300

    Description of classes of snakes: new functions, add function eating and move

--- model_objects.py
+++ model_objects.py
@@ -25,21 +25,55 @@ class Snakes:
     def __init__(self, coordinates):
         self.coordinates = coordinates
 
-    def collision(self, x_snake, y_snake):
+    def collision(self, coordinates):
+        '''
+        :param coordinates - массив координат квадратиков другой змейки
+        :return: 0 - дополнительная змейка врезалась в данную, 1 - наоборот, 2 - ничего не произошло
+        '''
         for position in self.coordinates:
             x = position[0]
             y = position[1]
-            if x == x_snake and y == y_snake:
-                return True
-        return False
+            if x == coordinates[0][0] and y == coordinates[0][1]:
+                return 0
+        for position in coordinates:
+            x = position[0]
+            y = position[1]
+            if x == self.coordinates[0][0] and y == self.coordinates[0][1]:
+                return 1
+        return 2
+
+    def move_tail(self):
+        for number in (1, len(self.coordinates) - 1, 1):
+            self.coordinates[number] = self.coordinates[number - 1]
 
 
 class Food(Snakes):
-    def __init__(self):
-        super(Food, self).__init__()
+    def __init__(self, coordinates):
+        Snakes.__init__(self, coordinates)
         self.color = (100, 100, 100)  # FIXME изменить на COLOR
+        self.actions = []
         self.live = True
-    def eating(self, x_snake, y_snake):
-        if:
+        self.mass = 5
+
+    def eating(self, coordinates):
+        '''
+        :param coordinates: координаты "побочной змейки"
+        :return: вывод прибавки к счёту по итогу обработки
+        '''
+        if self.collision(coordinates) == 0:
             self.live(False)
-        else:
+            return self.mass
+        return 0
+
+    def death(self, coordinates):
+        if self.collision(coordinates) == 1:
+            self.live(False)
+
+    def move(self):
+        self.actions.pop(0)
+
+
+class Enemy(Snakes):
+    def __init__(self, coordinates):
+        Snakes.__init__(self, coordinates)
+        self.color = (200, 200, 200)  # FIXME изменить на COLOR
