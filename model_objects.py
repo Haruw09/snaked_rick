901288bf1682345faa78aeab59a514e85b79e3ab
SixroomSandwich <konyshe.md@phystech.edu>
2021-12-19 16:11:55 +0300

    Теперь весь геймплей расположен в GameManager

--- model_objects.py
+++ model_objects.py
@@ -46,6 +46,9 @@ class GameManager:
         self.food = []
 
     def start_display_1(self):
+        '''
+        Функция занимается всем на первом предыгровом экане
+        '''
         screen_number = 1
         finished = False
         result = 0
@@ -67,16 +70,18 @@ class GameManager:
                     self.top_number = 0
 
         if result == 1:
-            self.walls = input.read_wall_data('levels\\Lvl_1.txt')
             self.top_number = 1
         elif result == 2:
-            self.walls = input.read_wall_data('levels\\Lvl_2.txt')
             self.top_number = 2
         elif result == 3:
-            self.walls = input.read_wall_data('levels\\Lvl_3.txt')
             self.top_number = 3
 
+        self.read_walls()
+
     def start_display_2(self):
+        '''
+        Функция занимается всем на втором предыгровом экане
+        '''
         finished = False
         result = 0
         while not finished and result == 0:
@@ -96,6 +101,87 @@ class GameManager:
             self.FPS = 30
             self.score_for_food = 3
 
+    def play_display(self):
+        self.read_main_snake()
+        self.read_food()
+
+        '''
+        Рисуем начальные положения всех объектов
+        Из всех змеек-еды выбираем одну с помощью рандома
+        '''
+        self.draw_walls()
+        for num in range(self.number_of_food):
+            self.new_food(num)
+        self.draw_food()
+
+        vis.draw_snake(self.main_snake.coordinates, self.main_snake.color, self.main_snake.head_color, self.screen)
+        vis.draw_field(self.screen, SIZE)
+
+        '''
+        Тут мы начинаем гонять нашу игру, пока змея не врежется в стену или не укусит сама себя
+        '''
+        self.choose_music()
+
+        finished = False
+        while not finished and self.main_snake.death == 0:
+            clock.tick(self.FPS)
+            '''
+            Создаём чистый экран
+            '''
+            pygame.display.update()
+            screen.fill(WHITE)
+            '''
+            Работаем со стенами (С каждой отдельно)
+            '''
+            self.draw_walls()
+            for wall in self.walls:
+                '''
+                Проверяем, ударилась ли главная змея со стеной
+                Если да, то главная змея умирает
+                '''
+                self.wall_collision(wall)
+                '''
+                Проверяем, дошла ли змея - еда до стены
+                Если да, то она развернётся
+                '''
+                self.food_turn(wall)
+            '''
+            Рисуем змейку-еду
+            '''
+            self.draw_food()
+            '''
+            Проверяем, скушала ли змея еду
+            Если да, то создаётся новая еда, а змейка вырастает на одну ячейку
+            '''
+            self.eating()
+            '''
+            Проверяем, не укусила ли змея сама себя
+            Если да, то змея умирает
+            '''
+            self.bite()
+
+            '''
+            Рисуем змею
+            '''
+            vis.draw_snake(self.main_snake.coordinates, self.main_snake.color, self.main_snake.head_color, self.screen)
+            '''
+            Проверяем нажатые клавиши
+            Если нажата клавиша w, a, s или d, то змея поворачивается в нужном направлении
+            '''
+            finished = self.wasd()
+
+            '''
+            Двигаем змею
+            Сначала хвост, потом голову
+            '''
+            self.main_snake.move_tail()
+            self.main_snake.move_head(self.main_snake.direction)
+            '''
+            Змея - еда двигается в зависимости от своей скорости
+            Еда двигается на клетку каждый 2 ход (game.food[food_number].miss + 1)
+            '''
+            self.move_food()
+
     def table_display(self):
         '''
         Здесь читается, анализируется, переписывается и записывается обратно в файл таблица лидеров
@@ -103,7 +189,7 @@ class GameManager:
         table = input.top_entry(self.score, self.name, self.top_number)
         finished = False
         '''
-        Здесь игрок модет полюбоваться таблицей лидеров
+        Здесь игрок может полюбоваться таблицей лидеров
         Закрыть окно можно, нажав любую кнопку
         '''
         bottom_pressed = False
@@ -117,20 +203,47 @@ class GameManager:
                     finished = control.update(event)
 
     def read_main_snake(self):
+        '''
+        Функция считывает информацию о главной змейке и заносит её в соответствующее место
+        '''
         self.main_snake = input.read_main_snake_data('other\\main_snake.txt')
 
     def read_food(self):
+        '''
+            Функция считывает информацию о змеях - еде и заносит её в соответствующее место
+        '''
         self.food = input.read_food_data('other\\food.txt')
 
+    def read_walls(self):
+        '''
+            Функция считывает информацию о стенах на нужном уровне и заносит её в соответствующее место
+        '''
+        if self.top_number == 1:
+            self.walls = input.read_wall_data('levels\\Lvl_1.txt')
+        elif self.top_number == 2:
+            self.walls = input.read_wall_data('levels\\Lvl_2.txt')
+        else:
+            self.walls = input.read_wall_data('levels\\Lvl_3.txt')
+
     def draw_walls(self):
+        '''
+        Функция рисует стены
+        '''
         for wall in self.walls:
             vis.draw_wall(wall.x_begin, wall.y_begin, wall.x_end, wall.y_end, wall.color, self.screen)
 
     def draw_food(self):
+        '''
+        Функция рисует еду
+        '''
         for num in self.live_food:
             vis.draw_snake(self.food[num].coordinates, self.food[num].color, self.food[num].head_color, self.screen)
 
     def new_food(self, num):
+        '''
+        Функция присваевает новый номер еды
+        num - порядковый номер еды в массиве live_food
+        '''
         flag = 0
         while flag == 0:
             self.live_food[num] = randint(0, len(self.food) - 1)
@@ -145,6 +258,9 @@ class GameManager:
 
 
     def choose_music(self):
+        '''
+        Функция выбирает рандомную песню и проигрывает её
+        '''
         rnd = randint(1, 3)
         if rnd == 1:
             pygame.mixer.music.load('music\\breaktime.mp3')
@@ -157,14 +273,25 @@ class GameManager:
             pygame.mixer.music.play(-1)
 
     def wall_collision(self, wall):
+        '''
+        Функция проверяет, ударилась ли главная змея в стену
+        wall - определённая стена из массива стен walls
+        '''
         if wall.collision(self.main_snake.coordinates[0][0], self.main_snake.coordinates[0][1]):
             self.main_snake.death = 1
 
     def food_turn(self, wall):
+        '''
+        Функция разворачивает еду, если та ударилась в стену
+        wall - стена из массива стен walls
+        '''
         for num in self.live_food:
             self.food[num].turn(wall.x_begin, wall.y_begin, wall.x_end, wall.y_end)
 
     def move_food(self):
+        '''
+        Функция двигает всю еду, которая только есть
+        '''
         for num in self.live_food:
             self.food[num].move_miss = (self.food[num].move_miss + 1) % (self.food[num].miss + 1)
             if self.food[num].move_miss == self.food[num].miss:
@@ -172,6 +299,10 @@ class GameManager:
                 self.food[num].move_head(self.food[num].direction)
 
     def eating(self):
+        '''
+        Функция проверяет, была ли съедена какая-либо еда
+        Если да, то главная змея удлиняется, начисляется определённое кол-во очков и создаётся новая змея
+        '''
         for num in range(self.number_of_food):
             food_number = self.live_food[num]
             if self.main_snake.collision(self.food[food_number].coordinates) == 1:
@@ -183,10 +314,16 @@ class GameManager:
                 self.new_food(num)
 
     def bite(self):
+        '''
+        Функция проверяет, укусила ли главная змея саму себя
+        '''
         if self.main_snake.collision(self.main_snake.coordinates) == 0:
             self.main_snake.death = 1
 
     def wasd(self):
+        '''
+        Функция поворачивает главную змея по кнопкам w, a, s и d
+        '''
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 self.main_snake.veer()
